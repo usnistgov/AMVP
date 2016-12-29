cefb9d96a4510fc72d278d39ca489d0b153bc5aa
Shawn Geddis <geddis@apple.com>
2016-12-28 20:52:41 -0800

    Initial Project Creation

---
+++ AMVP/AMVP/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AMVP
+//
+//  Created by Shawn Geddis on 12/28/16.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
